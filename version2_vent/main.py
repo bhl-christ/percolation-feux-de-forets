ca7132a9f37806809d4762725b540945682ae8ea
SemihAslan123 <semihaslan2210@gmail.com>
2026-01-09 16:10:46 +0100

    commit mainv2 modification

--- version2_vent/main.py
+++ version2_vent/main.py
@@ -5,14 +5,13 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    print("=== PROJET PERCOLATION - VERSION 2 (AVANCÉE) ===")
+    print("PROJET PERCOLATION - VERSION 2")
     print("1. Démonstration visuelle (Avec/Sans Vent)")
     print("2. Étude Comparative : Impact du Vent sur le Seuil Critique")
 
     choix = input("Votre choix (1 ou 2) : ")
 
     if choix == "1":
-        # Mode Démo
         try:
             d = float(input("Densité (ex: 0.65) : "))
         except:
@@ -31,21 +30,20 @@ def main():
         visualisation.animer_feu(foret, fct_update)
 
     elif choix == "2":
-        # Mode Scientifique
         n = 50
         nb_sim = 30
 
         print(f"\n--- DÉBUT DE L'ÉTUDE COMPARATIVE (n={n}) ---")
 
-        # 1. Calcul SANS vent (Référence V1)
+        # Calcul SANS vent (ref v1)
         x1, y1 = simulation.etude_percolation(n, nb_sim, vent_nord=False)
         seuil1 = simulation.trouver_seuil_critique(x1, y1)
 
-        # 2. Calcul AVEC vent (Evolution V2)
+        # Calcul AVEC vent (evolution v2)
         x2, y2 = simulation.etude_percolation(n, nb_sim, vent_nord=True)
         seuil2 = simulation.trouver_seuil_critique(x2, y2)
 
-        # 3. Rapport
+        # rapport
         print("\n" + "=" * 40)
         print("RÉSULTATS DE LA MODÉLISATION")
         print("=" * 40)
@@ -57,12 +55,12 @@ def main():
         print("Il faut environ 10% d'arbres en plus pour que le feu passe.")
         print("=" * 40)
 
-        # 4. Affichage Comparatif
+        # affichage comparatif
         plt.figure(figsize=(10, 6))
         plt.plot(x1, y1, 'b-o', label=f'Sans Vent (Isotrope) $p_c={seuil1:.2f}$', alpha=0.7)
         plt.plot(x2, y2, 'r-s', label=f'Avec Vent (Anisotrope) $p_c={seuil2:.2f}$')
 
-        # Zone de décalage
+        # zone de décalage
         plt.axvline(x=seuil1, color='blue', linestyle='--', alpha=0.3)
         plt.axvline(x=seuil2, color='red', linestyle='--', alpha=0.3)
         plt.axvspan(seuil1, seuil2, color='yellow', alpha=0.1, label='Impact du Vent')
