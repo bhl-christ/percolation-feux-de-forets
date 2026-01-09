2744c60539bb30332d847d08872bdd6e48778666
bhl-christ <bahouassilachrist@gmail.com>
2026-01-09 15:13:45 +0100

    Mise à jour de main.py et visualisation.py. Ajout de l'animation pour voir la propagation du feu

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
+import model
 import simulation
 import visualisation
 
 def main():
     print(" PROJET PERCOLATION ")
-    choix = input("Voulez-vous lancer :\n1. Une démonstration visuelle unique\n2. La courbe statistique complète\nVotre choix (1 ou 2) : ")
+    print("1. Démonstration visuelle")
+    print("2. Courbe statistique (Transition de Phase)")
+    choix = input("Votre choix (1 ou 2) : ")
     
     if choix == "1":
-        n = 100
-        d = float(input("Entrez la densité (ex: 0.60) : "))
+        try:
+            val_d = input("Entrez la densité (ex: 0.60) : ")
+            d = float(val_d)
+        except ValueError:
+            d = 0.60
+            print("Valeur incorrecte, utilisation de 0.60 par défaut.")
+            
+        n = 100 
         
-        print("Simulation en cours...")
-        percole, foret_finale = simulation.simuler_feu_complet(n, d)
+        print(f"Lancement de l'animation (n={n}, p={d})...")
+        print("Fermez la fenêtre du graphique pour quitter.")
         
-        resultat = "A PERCOLÉ (Le feu a traversé)" if percole else "N'A PAS PERCOLÉ"
-        print(f"Résultat : {resultat}")
+        # 1. On prépare la forêt
+        foret = model.generer_foret(n, d)
+        foret = model.allumer_feu(foret)
         
-        visualisation.afficher_foret(foret_finale, titre=f"Résultat (d={d}) : {resultat}")
+        # 2. On lance l'animation
+        # On passe la fonction 'etape_propagation' à l'animateur pour qu'il s'en serve
+        visualisation.animer_feu(foret, model.etape_propagation)
         
     elif choix == "2":
-        n = 50
-        nb_sim = 20
+        n = 50 
+        nb_sim = 20 
         
-        print("Calcul de la courbe en cours (patience)...")
+        print(f"Calcul de la courbe statistique (n={n}, {nb_sim} sims/point)...")
         x, y = simulation.etude_percolation(n, nb_simulations=nb_sim)
         
         visualisation.afficher_courbe_percolation(x, y)
